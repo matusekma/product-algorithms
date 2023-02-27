85e70ba4f0c70b1df8acc7e4a27df93b626be929
Marton Matusek <matusekma@gmail.com>
2023-02-27 09:54:34 +0100

    fix(division): fix division by 0 and test

--- src/main/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsTaskAService.kt
+++ src/main/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsTaskAService.kt
@@ -7,8 +7,14 @@ class CalculateProductsTaskAService : CalculateProductsService {
 
     override fun calculateProducts(numbers: List<Int>): List<Int> {
         validateNumberListLength(numbers)
-        val product = numbers.fold(1) { product, number -> product * number }
-        return numbers.map { product / it }
+        val countOfZeroes = numbers.count { it == 0 }
+        if (countOfZeroes > 1) return List(numbers.size) { 0 }
+
+        val productOfNonZeroes = numbers.fold(1) { product, number -> product * (if (number == 0) 1 else number) }
+        if (countOfZeroes == 1) {
+            return numbers.map { if (it == 0) productOfNonZeroes else 0 }
+        }
+        return numbers.map { productOfNonZeroes / it }
     }
 
 }
--- src/test/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsServiceTest.kt
+++ src/test/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsServiceTest.kt
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import java.util.stream.Stream
 
 @SpringJUnitConfig
 abstract class CalculateProductsServiceTest<T : CalculateProductsService> {
@@ -39,10 +43,46 @@ abstract class CalculateProductsServiceTest<T : CalculateProductsService> {
     }
 
     @Test
-    fun `given a list with more than 2 elements, it returns the products according to the algorithm`() {
-        val products = calculateProductsService.calculateProducts(listOf(1, 2, 3, 4))
+    fun `given the list contains one 0, it returns 0 except for the 0's index`() {
+        val products = calculateProductsService.calculateProducts(listOf(5, 6, 0))
 
-        assertEquals(listOf(24, 12, 8, 6), products)
+        assertEquals(listOf(0, 0, 30), products)
+    }
+
+    @Test
+    fun `given the list contains more than one 0, it returns all 0s`() {
+        val products = calculateProductsService.calculateProducts(listOf(0, 5, 6, 0))
+
+        assertEquals(listOf(0, 0, 0, 0), products)
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNumbersAndExpectedProducts")
+    fun `given a list with more than 2 elements, it returns the products according to the algorithm`(
+        numbers: List<Int>,
+        expectedProducts: List<Int>
+    ) {
+        val products = calculateProductsService.calculateProducts(numbers)
+
+        assertEquals(expectedProducts, products)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun getNumbersAndExpectedProducts(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                listOf(1, 2, 3, 4),
+                listOf(24, 12, 8, 6),
+            ),
+            Arguments.of(
+                listOf(11, 99, 23, 45),
+                listOf(99 * 23 * 45, 11 * 23 * 45, 11 * 99 * 45, 11 * 99 * 23),
+            ),
+            Arguments.of(
+                listOf(5, 0, 7, 2),
+                listOf(0, 5 * 7 * 2, 0, 0),
+            ),
+        )
     }
 
 }
