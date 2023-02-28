d90105b62fd13f3dad63be7b516eba1ba04596ea
Marton Matusek <matusekma@gmail.com>
2023-02-28 07:46:10 +0100

    fix(task-b): add Service annotation to task b

--- src/main/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsTaskBService.kt
+++ src/main/kotlin/hu/matusek/productalgorithms/feature/calculate/service/CalculateProductsTaskBService.kt
@@ -1,5 +1,8 @@
 package hu.matusek.productalgorithms.feature.calculate.service
 
+import org.springframework.stereotype.Service
+
+@Service
 class CalculateProductsTaskBService : CalculateProductsService {
 
     override fun calculateProducts(numbers: List<Int>): List<Int> {
